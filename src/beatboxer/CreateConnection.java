585833bae17e7bf911a487e798e015ea8a9d2bb3
Shivam Tayal <tayal.2shivam@gmail.com>
2016-10-24 12:20:31 +0530

    Update CreateConnection.java
    minor error removed

--- src/beatboxer/CreateConnection.java
+++ src/beatboxer/CreateConnection.java
@@ -4,7 +4,7 @@ public class CreateConnection {
 	public Connection con;
 	CreateConnection(){
 		try{
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306","root","123");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BeatBoxer","root","123");
 		}catch(Exception e){
 			
 		}
