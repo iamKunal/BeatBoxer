8fb30a60a56a4c623304c25ca231475a9d53a699
tayal007 <>
2016-11-04 00:24:49 +0530

    changed return type of search functions from resultset to bbitem and bbsong

--- src/beatboxer/Search.java
+++ src/beatboxer/Search.java
@@ -1,41 +1,42 @@
 package beatboxer;
 import java.sql.*;
+import javafx.collections.ObservableList;
 public class Search extends CreateConnection{
 	
-	public ResultSet SearchAlbum(String string){
+	public ObservableList<BBItem> SearchAlbum(String string){
 		try {
 			String sql = "select * from album WHERE albumname LIKE ?";
 			PreparedStatement statement = con.prepareStatement(sql);
 			statement.setString(1,'%'+string+'%');
 			ResultSet res=statement.executeQuery();
-			return res;
+			return BBGenerator.item(res);
 			}
 		 catch(Exception e) {
 			 return null;
 		}	
 	}
 	
-	public ResultSet SearchTrack(String string){
+	public ObservableList<BBSong> SearchTrack(String string){
 		try {
 			String sql = "select * from track NATURAL JOIN trackinfo" + 
 					"NATURAL JOIN artist NATURAL JOIN album WHERE trackname LIKE ?";
 			PreparedStatement statement = con.prepareStatement(sql);
 			statement.setString(1,"%"+string+"%");
 			ResultSet res=statement.executeQuery();
-			return res;
+			return BBGenerator.song(res);
 			}
 		 catch(Exception e) {
 		}
 		return null;	
 	}
 	
-	public ResultSet SearchArtist(String string){
+	public ObservableList<BBItem> SearchArtist(String string){
 		try {
 			String sql = "select * from artist WHERE artistname LIKE ?";
 			PreparedStatement statement = con.prepareStatement(sql);
 			statement.setString(1,'%'+string+'%');
 			ResultSet res=statement.executeQuery();
-			return res;
+			return BBGenerator.item(res);
 			}
 		 catch(Exception e) {
 			 return null;
