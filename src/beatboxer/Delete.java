38484648077c82bb4bb6511b67133b5f9e048912
Shivam Tayal <tayal.2shivam@gmail.com>
2016-11-05 00:31:47 +0530

    modified delete class for playlist

--- src/beatboxer/Delete.java
+++ src/beatboxer/Delete.java
@@ -1,42 +1,60 @@
 package beatboxer;
 
 import java.sql.*;
-public class Delete extends CreateConnection{
-	public void deleteTrack(int TrackId){
-		try{
-			String sql = "delete from track where trackid = ?";
-			PreparedStatement statement = con.prepareStatement(sql);
-			statement.setInt(1,TrackId);
-			statement.executeUpdate();
-			sql = "delete from trackinfo where trackid = ?";
-			statement = con.prepareStatement(sql);
-			statement.setInt(1,TrackId);
-			statement.executeUpdate();
-		}catch(SQLException e){
-			
-		}
-	}
-	public void deleteAlbum(int AlbumId){
-		try{
-			String sql = "delete from album where albumid = ?";
-			PreparedStatement statement = con.prepareStatement(sql);
-			statement.setInt(1,AlbumId);
-			statement.executeUpdate();
-			sql = "select trackid from trackinfo where albumid = ?";
-			PreparedStatement gettracks = con.prepareStatement(sql);
-			gettracks.setInt(1, AlbumId);
-			ResultSet res = gettracks.executeQuery();
-			while(res.next()){
-				deleteTrack(res.getInt("trackid"));
-			}
-		}catch(SQLException e){
-			
-		}
-	}
+
+public class Delete extends CreateConnection {
+
+    public void deleteTrack(int TrackId) {
+        try {
+            String sql = "delete from track where trackid = ?";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setInt(1, TrackId);
+            statement.executeUpdate();
+            sql = "delete from trackinfo where trackid = ?";
+            statement = con.prepareStatement(sql);
+            statement.setInt(1, TrackId);
+            statement.executeUpdate();
+            sql = "delete from playlistinfo where trackid = ?";
+            statement.setInt(1, TrackId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+
+        }
+    }
+
+    public void deleteArtist(int ArtistId) {
+        try {
+            String sql = "delete from artist where artistid = ?";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setInt(1, ArtistId);
+            statement.executeUpdate();
+            sql = "select trackid from trackinfo where artistid = ?";
+            PreparedStatement gettracks = con.prepareStatement(sql);
+            gettracks.setInt(1, ArtistId);
+            ResultSet res = gettracks.executeQuery();
+            while (res.next()) {
+                deleteTrack(res.getInt("trackid"));
+            }
+        } catch (SQLException e) {
+
+        }
+    }
+
+    public void deleteAlbum(int AlbumId) {
+        try {
+            String sql = "delete from album where albumid = ?";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setInt(1, AlbumId);
+            statement.executeUpdate();
+            sql = "select trackid from trackinfo where albumid = ?";
+            PreparedStatement gettracks = con.prepareStatement(sql);
+            gettracks.setInt(1, AlbumId);
+            ResultSet res = gettracks.executeQuery();
+            while (res.next()) {
+                deleteTrack(res.getInt("trackid"));
+            }
+        } catch (SQLException e) {
+
+        }
+    }
 }
-
-
-
-
-
-
