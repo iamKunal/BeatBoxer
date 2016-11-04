03aef62df6a04e12ddf323df0873091b2ad8538a
Shivam Tayal <tayal.2shivam@gmail.com>
2016-11-05 00:27:54 +0530

    show function

--- src/beatboxer/Directory.java
+++ src/beatboxer/Directory.java
@@ -1,24 +1,44 @@
 package beatboxer;
+
 import java.sql.*;
-public class Directory extends CreateConnection{
-    public void add(String Location){
+import java.util.ArrayList;
+
+public class Directory extends CreateConnection {
+
+    public void add(String Location) {
         String sql = "insert into directories(folderlocation) values(?)";
-        try{
+        try {
             PreparedStatement statement = con.prepareStatement(sql);
-            statement.setString(1,Location);
+            statement.setString(1, Location);
             statement.executeUpdate();
-        }catch(SQLException e){
-            
+        } catch (SQLException e) {
+
         }
     }
-    public void delete(String Location){
+
+    public void delete(String Location) {
         String sql = "delete from directories where folderlocation = ?";
-        try{
+        try {
             PreparedStatement statement = con.prepareStatement(sql);
-            statement.setString(1,Location);
+            statement.setString(1, Location);
             statement.executeUpdate();
-        }catch(SQLException e){
-            
+        } catch (SQLException e) {
+
         }
+    }
+
+    public ArrayList<String> Show() {
+        ArrayList<String> list = new ArrayList();
+        try {
+            Statement statement = con.createStatement();
+            ResultSet res = statement.executeQuery("select * from directories");
+            while (res.next()) {
+                list.add(res.getString("folderlocation"));
+            }
+            return list;
+        } catch (SQLException e) {
+
+        }
+        return null;
     }
 }
