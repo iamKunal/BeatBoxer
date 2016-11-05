cbe108950a9b61981f91e9b508f379e87d6b5424
Shivam Tayal <tayal.2shivam@gmail.com>
2016-11-06 01:42:33 +0530

    Bugs Resolved

--- src/beatboxer/Delete.java
+++ src/beatboxer/Delete.java
@@ -14,33 +14,30 @@ public class Delete extends CreateConnection {
             statement = con.prepareStatement(sql);
             statement.setInt(1, TrackId);
             statement.executeUpdate();
+            Statement st = con.createStatement();
+            ResultSet res = st.executeQuery("select artist.artistid from artist where (select count(*) from trackinfo where trackinfo.artistid = artist.artistid)=0");
+            while (res.next()) {
+                sql = "delete from artist where artistid = ?";
+                statement = con.prepareStatement(sql);
+                statement.setInt(1, res.getInt("artistid"));
+                statement.executeUpdate();
+            }
+            res = st.executeQuery("select album.albumid from album where (select count(*) from trackinfo where trackinfo.albumid = album.albumid)=0; ");
+            while (res.next()) {
+                sql = "delete from album where albumid = ?";
+                statement = con.prepareStatement(sql);
+                statement.setInt(1, res.getInt("albumid"));
+                statement.executeUpdate();
+            }
             sql = "delete from playlistinfo where trackid = ?";
             statement = con.prepareStatement(sql);
             statement.setInt(1, TrackId);
             statement.executeUpdate();
-            Statement st = con.createStatement();
-            ResultSet res = st.executeQuery("select artistid,count(*) from trackinfo group by artistid");
-            while(res.next()){
-                if(res.getInt("count(*)")==0){
-                    sql = "delete from artist where artistid = ?";
-                    statement = con.prepareStatement(sql);
-                    statement.setInt(1, res.getInt("artistid"));
-                    statement.executeUpdate();
-                }
-            }
-            res = st.executeQuery("select albumid,count(*) from trackinfo group by albumid");
-            while(res.next()){
-                if(res.getInt("count(*)")==0){
-                    sql = "delete from album where albumid = ?";
-                    statement = con.prepareStatement(sql);
-                    statement.setInt(1, res.getInt("albumid"));
-                    statement.executeUpdate();
-                }
-            }
         } catch (SQLException e) {
 
         }
     }
+
 
     public void deleteArtist(int ArtistId) {
         try {
