41f74e50820b61ae20bc9dbb3a4d2c3ac2d7cae9
Shivam Tayal <tayal.2shivam@gmail.com>
2016-11-06 21:01:25 +0530

    modified search - now the results are shown in alphabetical order

--- src/beatboxer/Search.java
+++ src/beatboxer/Search.java
@@ -1,44 +1,43 @@
 package beatboxer;
+
 import java.sql.*;
 import javafx.collections.ObservableList;
-public class Search extends CreateConnection{
-	
-	public ObservableList<BBItem> SearchAlbum(String string){
-		try {
-			String sql = "select * from album WHERE albumname LIKE ?";
-			PreparedStatement statement = con.prepareStatement(sql);
-			statement.setString(1,'%'+string+'%');
-			ResultSet res=statement.executeQuery();
-			return BBGenerator.item(res);
-			}
-		 catch(Exception e) {
-			 return null;
-		}	
-	}
-	
-	public ObservableList<BBSong> SearchTrack(String string){
-		try {
-			String sql = "select * from track natural join artist natural join album natural join trackinfo where trackname LIKE ?";
-			PreparedStatement statement = con.prepareStatement(sql);
-			statement.setString(1,"%"+string+"%");
-			ResultSet res=statement.executeQuery();
-			return BBGenerator.song(res);
-			}
-		 catch(Exception e) {
-		}
-		return null;	
-	}
-	
-	public ObservableList<BBItem> SearchArtist(String string){
-		try {
-			String sql = "select * from artist WHERE artistname LIKE ?";
-			PreparedStatement statement = con.prepareStatement(sql);
-			statement.setString(1,'%'+string+'%');
-			ResultSet res=statement.executeQuery();
-			return BBGenerator.item(res);
-			}
-		 catch(Exception e) {
-			 return null;
-		}	
-	}
+
+public class Search extends CreateConnection {
+
+    public ObservableList<BBItem> SearchAlbum(String string) {
+        try {
+            String sql = "select * from album WHERE albumname LIKE ? order by albumname";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, '%' + string + '%');
+            ResultSet res = statement.executeQuery();
+            return BBGenerator.item(res);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public ObservableList<BBSong> SearchTrack(String string) {
+        try {
+            String sql = "select * from track natural join artist natural join album natural join trackinfo where trackname LIKE ? order by trackname";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, "%" + string + "%");
+            ResultSet res = statement.executeQuery();
+            return BBGenerator.song(res);
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public ObservableList<BBItem> SearchArtist(String string) {
+        try {
+            String sql = "select * from artist WHERE artistname LIKE ? order by artistname";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, '%' + string + '%');
+            ResultSet res = statement.executeQuery();
+            return BBGenerator.item(res);
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
